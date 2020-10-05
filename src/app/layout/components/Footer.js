63d2a3b8dfc926ecdb9e4c15a548678eca99846d
tharinduRE <42732265+tharinduRE@users.noreply.github.com>
2020-10-05 20:49:54 +0530

    added about view

--- src/app/layout/components/Footer.js
+++ src/app/layout/components/Footer.js
@@ -1,11 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
+import { Button, Modal } from "react-bootstrap";
 
 export default function Footer() {
   const today = new Date().getFullYear();
 
+  const [showModel, setShowModel] = useState(false);
+
+  const handleClose = () => setShowModel(false);
+  const handleShow = () => setShowModel(true);
+
   return (
     <>
       {/* begin::Footer */}
+      <Modal 
+        show={showModel}
+        onHide={handleClose}
+        size="lg"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+        >
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
+
       <div className="footer footer-fixed bg-white py-4 d-flex flex-lg-column">
         {/* begin::Container */}
         <div className="container-fluid d-flex flex-column flex-md-row align-items-center justify-content-between">
@@ -28,14 +52,7 @@ export default function Footer() {
           {` `}
           {/* begin::Nav */}
           <div className="nav nav-dark order-1 order-md-2">
-            <a
-              href="/about"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="nav-link pr-3 pl-0"
-            >
-              About
-            </a>
+            <a href="void:" onClick={handleShow} className="nav-link pr-3 pl-0">About</a>
           </div>
           {/* end::Nav */}
         </div>
