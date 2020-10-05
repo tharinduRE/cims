2ea05f94ed1b83997abdea2ddd89c95bf92f7c28
tharinduRE <42732265+tharinduRE@users.noreply.github.com>
2020-10-05 22:34:01 +0530

    fixed className issue

--- src/app/layout/components/Brand.js
+++ src/app/layout/components/Brand.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Brand() {
   return (
     <>
-      <div class="aside-brand d-flex flex-column align-items-center flex-column-auto mh-100 py-lg-2 bg-white">
+      <div className="aside-brand d-flex flex-column align-items-center flex-column-auto mh-100 py-lg-2 bg-white">
         <a href="/">
           <img
             src={process.env.PUBLIC_URL + "/images/pera-logo.png"}
