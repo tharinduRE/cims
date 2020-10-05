bb11aa71e7e322d38a67f8bddec1483c4e639fb9
tharinduRE <42732265+tharinduRE@users.noreply.github.com>
2020-10-05 23:35:26 +0530

    error routing fixed

--- src/app/Routes.js
+++ src/app/Routes.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import Switch from 'react-bootstrap/esm/Switch'
+import { Route } from 'react-router-dom'
 import BasePage from './Basepage'
 import Layout from './layout/Layout'
+import { ErrorPage } from './pages/errors/ErrorPage'
 /*
     This one handle routes based on authentication
 */
@@ -10,6 +12,9 @@ export default function Routes() {
 
     return (
         <Switch>
+
+            <Route path="/error" component={ErrorPage}/>
+
             <Layout>
                 <BasePage/>
             </Layout>
