b49ddd0a2411f10320aa1abdca98905e68c247f2
tharinduRE <42732265+tharinduRE@users.noreply.github.com>
2020-10-07 23:27:25 +0530

    added item service methods

--- src/app/service/itemService.js
+++ src/app/service/itemService.js
@@ -1,14 +1,38 @@
-import axios from './Axios';
+import axios from "./Axios";
 
 const get = (id) => {
-    return axios.get(`/item-stocks/${id}`);
-}
+  return axios.get(`/item-stocks/${id}`);
+};
 
-const getAll = () => {
-    return axios.get(`/item-stocks/`)
-}
+const getAll = (store) => {
+  switch (store) {
+    case "ORG":
+      return axios.get("/item-stocks?stockStore.in=ORG");
+    case "INORG":
+      return axios.get("/item-stocks?stockStore.in=INORG");
+    default:
+      return axios.get(`/item-stocks/`);
+  }
+};
 
-export default  {
-    get,
-    getAll,
-}
+const getLowAll = (store, value) => {
+  return axios.get(
+    `/item-stocks?stockStore.in=${store}&totalQuantity.lessThanOrEqual=${value}`
+  );
+};
+
+const count = (store) => {
+  return axios.get(`/item-stocks/count?stockStore.in=${store}`);
+};
+
+const add = (data) => {
+  return axios.post(`/item-stocks/`, data);
+};
+
+export default {
+  get,
+  getAll,
+  count,
+  getLowAll,
+  add,
+};
