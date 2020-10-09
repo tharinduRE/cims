61b7c9ded6a4abeafa11c17d980ebbc7969a75bb
tharinduRE <42732265+tharinduRE@users.noreply.github.com>
2020-10-09 22:51:14 +0530

    added react suspense

--- src/app/App.js
+++ src/app/App.js
@@ -1,12 +1,15 @@
-import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
-import Routes from './Routes';
+import React, { Suspense } from "react";
+import { BrowserRouter } from "react-router-dom";
+import Routes from "./Routes";
+import SplashScreen from "./layout/SplashScreen";
 
-function App({store}) {
+function App({ store }) {
   return (
-    <BrowserRouter>
-          <Routes/>
-    </BrowserRouter>
+    <Suspense fallback={<SplashScreen/>}>
+      <BrowserRouter>
+        <Routes />
+      </BrowserRouter>
+    </Suspense>
   );
 }
 
---
+++ src/app/layout/SplashScreen.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Spinner } from 'react-bootstrap'
+
+export default function SplashScreen() {
+    return (
+        <div>
+           <Spinner/> Looading ...
+        </div>
+    )
+}
