8af18f1eb38b55ba9be5cac8659a3fccfeb9c18f
tharinduRE <42732265+tharinduRE@users.noreply.github.com>
2020-10-10 22:33:44 +0530

    Updated to react router NavLink

--- src/app/layout/components/AsideMenu.js
+++ src/app/layout/components/AsideMenu.js
@@ -1,63 +1,65 @@
-import React from 'react'
+import React from "react";
 /* import { OverlayTrigger, Tooltip } from "react-bootstrap";
- */import { BsHouse, BsSearch, BsApp, BsPlus,BsList } from "react-icons/bs";
+ */ import { BsHouse, BsSearch, BsApp, BsPlus, BsList } from "react-icons/bs";
+import { NavLink } from "react-router-dom";
 
 export default function AsideMenu() {
-    return (
-        <ul className="nav flex-column">
-          <li
-            className="nav-item mb-5"
-            data-toggle="tooltip"
-            data-placement="right"
-            data-container="body"
-            data-boundary="window"
-            title=""
-          >
-            {/* <OverlayTrigger
+  return (
+    <ul className="nav flex-column">
+      <li
+        className="nav-item mb-5"
+        data-toggle="tooltip"
+        data-placement="right"
+        data-container="body"
+        data-boundary="window"
+        title=""
+      >
+        {/* <OverlayTrigger
               placement="right"
               overlay={<Tooltip id="tooltip-right">Search</Tooltip>}
             > */}
-              <a
-                href="/"
-                className="nav-link btn btn-icon btn-clean btn-icon-white btn-lg active"
-              >
-                <BsSearch />
-              </a>
-{/*             </OverlayTrigger>
- */}          </li>
-          <li className="nav-item mb-5">
-            <a
-              href="/home"
-              className="nav-link btn btn-icon btn-clean btn-icon-white btn-lg active"
-            >
-              <BsHouse />
-            </a>
-          </li>
-          <li className="nav-item mb-5">
-            <a
-              href="/listall"
-              className="nav-link btn btn-icon btn-clean btn-icon-white btn-lg active"
-            >
-              <BsList />
-            </a>
-          </li>
+        <NavLink
+          to={{ pathname: "/search" }}
+          className="nav-link btn btn-icon btn-clean btn-icon-white btn-lg active"
+        >
+          <BsSearch />
+        </NavLink>
+        {/*             </OverlayTrigger>
+         */}{" "}
+      </li>
+      <li className="nav-item mb-5">
+        <NavLink
+          to="/home"
+          className="nav-link btn btn-icon btn-clean btn-icon-white btn-lg active"
+        >
+          <BsHouse />
+        </NavLink>
+      </li>
+      <li className="nav-item mb-5">
+        <NavLink
+          to="/listall"
+          className="nav-link btn btn-icon btn-clean btn-icon-white btn-lg active"
+        >
+          <BsList />
+        </NavLink>
+      </li>
 
-          <li className="nav-item mb-5">
-            <a
-              href="/order"
-              className="nav-link btn btn-icon btn-clean btn-icon-white btn-lg active"
-            >
-              <BsApp />
-            </a>
-          </li>
-          <li className="nav-item mb-5">
-            <a
-              href="/addItem"
-              className="nav-link btn btn-icon btn-clean btn-icon-white btn-lg active"
-            >
-              <BsPlus />
-            </a>
-          </li>
-        </ul>
-    )
+      <li className="nav-item mb-5">
+        <NavLink
+          to="/order"
+          className="nav-link btn btn-icon btn-clean btn-icon-white btn-lg active"
+        >
+          <BsApp />
+        </NavLink>
+      </li>
+      <li className="nav-item mb-5">
+        <NavLink
+          to="/addItem"
+          className="nav-link btn btn-icon btn-clean btn-icon-white btn-lg active"
+        >
+          <BsPlus />
+        </NavLink>
+      </li>
+    </ul>
+  );
 }
