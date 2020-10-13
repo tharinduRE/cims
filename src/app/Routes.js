585371c0cf5090dd9f2196fa30774cde3db8ba49
tharinduRE <42732265+tharinduRE@users.noreply.github.com>
2020-10-13 23:59:23 +0530

    fixed switch issue :

--- src/app/Routes.js
+++ src/app/Routes.js
@@ -1,23 +1,21 @@
-import React from 'react'
-import Switch from 'react-bootstrap/esm/Switch'
-import { Route } from 'react-router-dom'
-import BasePage from './Basepage'
-import Layout from './layout/Layout'
-import { ErrorPage } from './pages/errors/ErrorPage'
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import BasePage from "./Basepage";
+import Layout from "./layout/Layout";
+import { ErrorPage } from "./pages/errors/ErrorPage";
+
 /*
     This one handle routes based on authentication
 */
 
 export default function Routes() {
-
     return (
         <Switch>
-
-            <Route path="/error" component={ErrorPage}/>
+            <Route path="/error" component={ErrorPage} />
 
             <Layout>
-                <BasePage/>
+                <BasePage />
             </Layout>
         </Switch>
-    )
+    );
 }
