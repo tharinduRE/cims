595393ce70cb019c848e2ee99d7e03b68670f5f5
tharinduRE <42732265+tharinduRE@users.noreply.github.com>
2020-10-24 18:11:47 +0530

    added formik

--- src/app/pages/auth/Login.js
+++ src/app/pages/auth/Login.js
@@ -2,71 +2,89 @@ import React from "react";
 import { Form, Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import { AuthContext } from "./AuthProvider";
+import { Formik } from "formik";
+import * as Yup from "yup";
 
 export default function Login() {
-    const { dispatch } = React.useContext(AuthContext);
+  const { dispatch } = React.useContext(AuthContext);
 
-    const payload = {
-        user: {
-            id:1,
-            authStores:["ORG","INORG"],
-            authRoles : ["ROLE_ADMIN","ROLE_USER"],
-            firstName: "Tharindu",
-            lastName: "Premasiri",
-            postTitle: "Admin",
-            email: "admin@system.com",
-            department: "Department of Chemistry",
-        },
-        token: 12314,
-    };
+  const payload = {
+    user: {
+      id: 1,
+      authStores: ["ORG", "INORG"],
+      authRoles: ["ROLE_ADMIN", "ROLE_USER"],
+      firstName: "Tharindu",
+      lastName: "Premasiri",
+      postTitle: "Admin",
+      email: "admin@system.com",
+      department: "Department of Chemistry",
+    },
+    token: 12314,
+  };
 
-    const handleLogin = (e) => {
-        e.preventDefault();
-        dispatch({ type: "LOGIN", payload: payload });
-    };
+  const LoginSchema = Yup.object().shape({
+    email: Yup.string().email("Invalid email").required("Required"),
+    password: Yup.string().required("required"),
+  });
 
-    return (
-        <>
-            <div className="mb-20">
-                <h3>Sign In To Admin</h3>
-                <div className="text-muted font-weight-bold">
-                    Enter your details to login to your account:
-                </div>
-            </div>
-            <Form onSubmit={(e) => handleLogin(e)}>
-                <Form.Group>
-                    <Form.Control
-                        type="text"
-                        placeholder="Email"
-                        className="form-control h-auto form-control-solid py-4 px-8"
-                    />
-                </Form.Group>
+  const handleLogin = (values) => {  
+      console.log(values);
+    dispatch({ type: "LOGIN", payload: payload });
+  };
 
-                <Form.Group>
-                    <Form.Control
-                        type="password"
-                        placeholder="Password"
-                        className="form-control h-auto form-control-solid py-4 px-8"
-                    />
-                </Form.Group>
-                <Button
-                    type="submit"
-                    variant="info"
-                    className="font-weight-bold px-9 py-4 my-3 mx-4"
-                >
-                    Login
-                </Button>
-            </Form>
+  return (
+    <>
+      <div className="mb-20">
+        <h3>Sign In To Admin</h3>
+        <div className="text-muted font-weight-bold">
+          Enter your details to login to your account:
+        </div>
+      </div>
+      <Formik
+        validationSchema={LoginSchema}
+        initialValues={{ email: "admin@system.com", password: "12345" }}
+        onSubmit={(values) => handleLogin(values)}
+      >
+        {({ handleSubmit, handleChange, handleBlur, values, touched, isValid, errors }) => (
+          <Form noValidate onSubmit={handleSubmit}>
+            <Form.Group>
+              <Form.Control
+                type="text"
+                name="email"
+                placeholder="Email"
+                value={values.email}
+                onChange={handleChange}
+                isInvalid={!!errors.email}
+                className="form-control h-auto form-control-solid py-4 px-8"
+              />
+              <Form.Control.Feedback type="invalid">{errors.email}</Form.Control.Feedback>
+            </Form.Group>
 
-            <div className="mt-10">
-                <span className="opacity-70 mr-4">Trouble Login !!! </span>
-                <Link
-                    to="/auth/login-help"
-                    className="text-muted text-hover-primary font-weight-bold"
-                >
-                    Help!
-                </Link>
-            </div>
-        </>
-    );
+            <Form.Group>
+              <Form.Control
+                type="password"
+                placeholder="Password"
+                name="password"
+                value={values.password}
+                onChange={handleChange}
+                isInvalid={!!errors.password}
+                className="form-control h-auto form-control-solid py-4 px-8"
+              />
+              <Form.Control.Feedback type="invalid">{errors.password}</Form.Control.Feedback>
+            </Form.Group>
+            <Button type="submit" variant="info" className="font-weight-bold px-9 py-4 my-3 mx-4">
+              Login
+            </Button>
+          </Form>
+        )}
+      </Formik>
+
+      <div className="mt-10">
+        <span className="opacity-70 mr-4">Trouble Login !!! </span>
+        <Link to="/auth/login-help" className="text-muted text-hover-primary font-weight-bold">
+          Help!
+        </Link>
+      </div>
+    </>
+  );
 }
--- src/app/views/ItemUpdate.js
+++ src/app/views/ItemUpdate.js
@@ -1,43 +1,31 @@
 import React, { useEffect, useState } from "react";
-import {
-  Form,
-  Button,
-  Col,
-  Row,
-  Card,
-  Alert,
-} from "react-bootstrap";
+import { Form, Button, Col, Row, Card, InputGroup, Spinner } from "react-bootstrap";
 import { toast } from "react-toastify";
 import itemService from "../service/itemService";
 import utilSerivice from "../service/utilService";
-import {AuthContext} from '../pages/auth/AuthProvider'
-import {getStoreName} from '../_helpers/StoreNameHelper'
+import { AuthContext } from "../pages/auth/AuthProvider";
+import { getStoreName } from "../_helpers/StoreNameHelper";
+import { Formik } from "formik";
+import * as Yup from "yup";
 
 export default function ItemUpdate({ id, update, onComplete, popUp }) {
-
   const { state: authState } = React.useContext(AuthContext);
 
-  const stores = authState.user.authStores;  const [measUnit, setmeasUnit] = useState([]);
+  const stores = authState.user.authStores;
+  const [measUnit, setmeasUnit] = useState([]);
 
   const notify = () => toast.success("Updated!");
 
-  const intialItem = {
-    itemCapacity: 200,
+  const [itemStock, setitemStock] = useState({
+    itemCapacity: 1,
     itemName: "",
     unitPrice: 1,
-    invStorageId: 1,
     itemStatus: "NEW",
+    totalQuantity: 1,
     stockStore: "ORG",
     storageUnitId: 1,
-    totalQuantity: 1,
-  };
-
-  const [itemStock, setitemStock] = useState(intialItem);
+  });
   const [submitted, setSubmitted] = useState(false);
-
-  const handleInputChange = (e) => {
-    setitemStock({ ...itemStock, [e.target.name]: e.target.value });
-  };
 
   const getItem = (id) => {
     itemService
@@ -61,9 +49,9 @@ export default function ItemUpdate({ id, update, onComplete, popUp }) {
       });
   };
 
-  const saveItemStock = () => {
+  const saveItemStock = (values) => {
     const payload = {
-      ...itemStock,
+      ...values,
       invStorageId: 1,
     };
 
@@ -72,16 +60,15 @@ export default function ItemUpdate({ id, update, onComplete, popUp }) {
       .then((response) => {
         console.log(response.data);
         setSubmitted(true);
-        resetForm();
       })
       .catch((err) => {
         console.log(err);
       });
   };
 
-  const updateItemStock = (id) => {
+  const updateItemStock = (values) => {
     const payload = {
-      ...itemStock,
+      ...values,
       id: id,
     };
 
@@ -95,164 +82,170 @@ export default function ItemUpdate({ id, update, onComplete, popUp }) {
       });
   };
 
-  const resetForm = () => {
-    setitemStock({ ...intialItem });
-  };
-
   useEffect(() => {
     if (id) {
       getItem(id);
     }
     if (submitted) {
-      onComplete(true);
       notify();
     }
     getMeasUnit();
   }, [id, submitted, onComplete]);
 
+  const schema = Yup.object().shape({
+    itemName: Yup.string().required("Valid Name is required"),
+    unitPrice: Yup.number().required("Price must entered"),
+    stockStore: Yup.string().required(),
+    totalQuantity: Yup.number().required(""),
+  });
+
   return (
     <>
-      {/* Only show submission confirm if not open in a popup */}
-      
-      {submitted && !popUp ? (
-        <Card>
-          <Alert variant="success">
-            Submiited.
-            <Button variant="link" onClick={() => setSubmitted(false)}>
-              Add another item
-            </Button>
-          </Alert>
-          <Card.Body>
-            <Row>
-              <Col>
-                Item : {itemStock.itemName}
-                Quantity : {itemStock.totalQuantity}
-              </Col>
-            </Row>
-          </Card.Body>
-        </Card>
-      ) : (
-        <Card>
-          <Card.Header className="border-0">
-            <Card.Title>
-              <h3 className="card-label font-size-h3 font-weight-bolder text-dark">
-                Add New Product
-              </h3>
-              <span className="text-muted mt-5 font-weight-bolder font-size-lg">
-                Add new item to inventory
-              </span>
-            </Card.Title>
-          </Card.Header>
-          <Card.Body className="pt-0">
-            <Form
-              onSubmit={
-                update
-                  ? (e) => {
-                      e.preventDefault();
-                      updateItemStock(id);
-                    }
-                  : (e) => {
-                      e.preventDefault();
-                      saveItemStock();
-                    }
-              }
-            >
-              <Form.Group>
-                <Form.Label>Item Name</Form.Label>
-                <Form.Control
-                  required
-                  type="text"
-                  name="itemName"
-                  placeholder="Item Name"
-                  onChange={handleInputChange}
-                  value={itemStock.itemName}
-                />
-                <Form.Text className="text-muted">
-                  Enter full item name as appeared in container
-                </Form.Text>
-              </Form.Group>
-              <Form.Group className="my-5">
-              <Form.Label>Item Store</Form.Label>
-                    <Form.Control
-                      as="select"
-                      name="stockStore"
-                      onChange={handleInputChange}
-                      value={itemStock.stockStore}
-                    >
-                      {stores.map((store, idx) => (
-                        <option key={idx}>{getStoreName(store)}</option>
-                      ))}
-                    </Form.Control>
-              </Form.Group>
-              <Row>
-                <Col>
-                  <Form.Group controlId="exampleForm.ControlInput1">
-                    <Form.Label>Item Quantity</Form.Label>
-                    <Form.Control
-                      type="number"
-                      name="totalQuantity"
-                      placeholder="Item Quantity"
-                      onChange={handleInputChange}
-                      value={itemStock.totalQuantity}
-                    />
-                  </Form.Group>
-                </Col>
-                <Col>
-                  <Form.Group className="mb-8">
-                    <Form.Label>Item Capacity</Form.Label>
-                    <Form.Control
-                      as="select"
-                      name="itemCapacity"
-                      onChange={handleInputChange}
-                      value={itemStock.itemCapacity}
-                    >
-                      {[1, 200, 500, 250].map((cap, idx) => (
-                        <option key={idx}>{cap}</option>
-                      ))}
-                    </Form.Control>
-                  </Form.Group>
-                </Col>
-                <Col>
-                  <Form.Group className="mb-8">
-                    <Form.Label>Unit</Form.Label>
-                    <Form.Control
-                      as="select"
-                      name="storageUnit"
-                      onChange={() => handleInputChange}
-                      value={itemStock.storageUnitId}
-                    >
-                      {measUnit.map((measUnit, index) => (
-                        <option key={index}>{measUnit.measUnit}</option>
-                      ))}
-                    </Form.Control>
-                  </Form.Group>
-                </Col>
-              </Row>
-              <Form.Group className="mb-8">
-                <Form.Label>Unit Price</Form.Label>
-                <Form.Control
-                  type="text"
-                  name="unitPrice"
-                  placeholder="Unit Price"
-                  onChange={handleInputChange}
-                  value={itemStock.unitPrice}
-                />
-                <Form.Control.Feedback type="invalid">
-                  Please choose a username.
-                </Form.Control.Feedback>
-              </Form.Group>
+      <Card>
+        <Card.Header className="border-0">
+          <Card.Title>
+            <h3 className="card-label font-size-h3 font-weight-bolder text-dark">
+              Add New Product
+            </h3>
+            <span className="text-muted mt-5 font-weight-bolder font-size-lg">
+              Add new item to inventory
+            </span>
+          </Card.Title>
+        </Card.Header>
+        <Card.Body className="pt-0">
+          <Formik
+            validationSchema={schema}
+            initialValues={itemStock}
+            enableReinitialize={true}
+            onSubmit={(values) => {
+              update ? updateItemStock(values) : saveItemStock(values);
+            }}
+          >
+            {({
+              handleSubmit,
+              handleChange,
+              handleReset,
+              isSubmitting,
+              values,
+              touched,
+              isValid,
+              errors,
+            }) => (
+              <Form noValidate onSubmit={handleSubmit}>
+                <Form.Group>
+                  <Form.Label>Item Name</Form.Label>
+                  <Form.Control
+                    required
+                    type="text"
+                    name="itemName"
+                    placeholder="Item Name"
+                    onChange={handleChange}
+                    value={values.itemName}
+                    isInvalid={errors.itemName}
+                  />
+                  <Form.Control.Feedback type="invalid">{errors.itemName}</Form.Control.Feedback>
+                  <Form.Text className="text-muted">
+                    Enter full item name as appeared in container
+                  </Form.Text>
+                </Form.Group>
+                <Form.Group className="my-5">
+                  <Form.Label>Item Store</Form.Label>
+                  <Form.Control
+                    as="select"
+                    name="stockStore"
+                    onChange={handleChange}
+                    value={values.stockStore}
+                  >
+                    {stores.map((store, idx) => (
+                      <option key={idx}>{getStoreName(store)}</option>
+                    ))}
+                  </Form.Control>
+                </Form.Group>
+                <Row>
+                  <Col>
+                    <Form.Group controlId="exampleForm.ControlInput1">
+                      <Form.Label>Item Quantity</Form.Label>
+                      <Form.Control
+                        type="number"
+                        name="totalQuantity"
+                        placeholder="Item Quantity"
+                        onChange={handleChange}
+                        value={values.totalQuantity}
+                      />
+                    </Form.Group>
+                  </Col>
+                  <Col>
+                    <Form.Group className="mb-8">
+                      <Form.Label>Item Capacity</Form.Label>
 
-              <Button variant="info" type="submit">
-                {update ? "Update" : "Save"}
-              </Button>
-              <Button variant="clear" onClick={onComplete}>
-                {" "}
-                Cancel{" "}
-              </Button>
-            </Form>
-          </Card.Body>
-        </Card>
-      )}
+                      <Form.Control
+                        type="number"
+                        name="itemCapacity"
+                        onChange={handleChange}
+                        value={values.itemCapacity}
+                      />
+                    </Form.Group>
+                  </Col>
+                  <Col>
+                    <Form.Group className="mb-8">
+                      <Form.Label>Unit</Form.Label>
+                      <Form.Control
+                        as="select"
+                        name="storageUnit"
+                        onChange={() => handleChange}
+                        value={itemStock.storageUnitId}
+                      >
+                        {measUnit.map((measUnit, index) => (
+                          <option key={index}>{measUnit.measUnit}</option>
+                        ))}
+                      </Form.Control>
+                    </Form.Group>
+                  </Col>
+                </Row>
+                <Form.Group className="mb-8">
+                  <Form.Label>Unit Price</Form.Label>
+                  <InputGroup className="mb-3">
+                    <InputGroup.Prepend>
+                      <InputGroup.Text id="basic-addon1">Rs.</InputGroup.Text>
+                    </InputGroup.Prepend>
+                    <Form.Control
+                      type="text"
+                      name="unitPrice"
+                      placeholder="Unit Price"
+                      onChange={handleChange}
+                      value={values.unitPrice}
+                      isInvalid={errors.unitPrice}
+                    />
+                  </InputGroup>
+                  <Form.Control.Feedback type="invalid">{errors.unitPrice}</Form.Control.Feedback>
+                </Form.Group>
+
+                <Button variant="info" type="submit">
+                  {update ? "Update" : "Save"}
+                  {isSubmitting && !submitted ? (
+                    <Spinner
+                      as="span"
+                      animation="border"
+                      size="sm"
+                      role="status"
+                      aria-hidden="true"
+                    />
+                  ) : (
+                    ""
+                  )}
+                </Button>
+                <Button variant="outline-info" onClick={handleReset}>
+                  Reset
+                </Button>
+                <Button variant="clear" onClick={onComplete}>
+                  {" "}
+                  Cancel{" "}
+                </Button>
+              </Form>
+            )}
+          </Formik>
+        </Card.Body>
+      </Card>
     </>
   );
 }
