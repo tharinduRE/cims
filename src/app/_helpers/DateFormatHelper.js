12c4dbf9990511849d8b5a7056d492200329990b
tharinduRE <42732265+tharinduRE@users.noreply.github.com>
2020-10-27 23:58:06 +0530

    date format fixes

--- src/app/_helpers/DateFormatHelper.js
+++ src/app/_helpers/DateFormatHelper.js
@@ -1,7 +1,9 @@
 import {format,formatDistanceToNow} from "date-fns";
 
+export const APP_LOCAL_DATETIME_FORMAT_Z = "do MMM yy, hh:mm aaaa";
+
 export const formatDate = (date) => {
-    return format(new Date(date), "do MMM yy, hh:mm aaaa");
+    return format(new Date(date), APP_LOCAL_DATETIME_FORMAT_Z);
 }
 
 export const timeAgo = (date) => {
